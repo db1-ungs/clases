c60aa315a0e2c067797c2c6497ab8eda95389ae7
Lucifer <rondelli@outlook.com>
2021-06-20 12:55:41 -0300

    Fix function examples

--- present/src/add_sub.go
+++ present/src/add_sub.go
@@ -11,9 +11,17 @@ func AddAndSub(i, j int) (int, int) {
 	return i + j, i - j
 }
 
+func AddSubAndDiv(i int, j int, k float64) (int, int, float64) {
+	return i + j, i - j, float64(i) / k
+}
+
 func main() {
 	a, b := AddAndSub(3, 5)
-	c, _ := AddAndSub(8, 2)
 	fmt.Printf("%d %d\n", a, b)
+
+	c, _ := AddAndSub(8, 2)
 	fmt.Printf("%d\n", c)
+
+	_, _, x := AddSubAndDiv(1, 3, 2)
+	fmt.Printf("%f\n", x)
 }
