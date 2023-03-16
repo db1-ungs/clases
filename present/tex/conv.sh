6d51456a9f3732238c2b14928bd62346ba7c12ae
Lucifer! <rondelli@users.noreply.github.com>
2023-03-16 12:59:44 -0300

    Fix dp tex

--- present/tex/conv.sh
+++ present/tex/conv.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
-context $1.tex
-pdfcrop $1.pdf
-convert -density 300 $1-crop.pdf -quality 90 $1.png
+# context $1.tex
+
+# Usage:
+#
+# ./conv.sh latex filename-without-extension
+#
+# or
+#
+# ./conv.sh context filename-without-extension
+
+$1 $2.tex
+pdfcrop $2.pdf
+convert -density 300 $2-crop.pdf -quality 90 $2.png
